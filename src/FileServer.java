aad0f57307a8bedb163ac2f3c1b51d50e263a574
yangshuobupt <867061644@qq.com>
2021-06-14 17:40:00 +0800

    yang

---
+++ src/FileServer.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.math.RoundingMode;
+import java.net.ServerSocket;
+import java.text.DecimalFormat;
+
+public class FileServer extends ServerSocket {
+    private static final int SERVER_PORT = 8899;
+
+    private static DecimalFormat df = null;
+
+    static {
+        // 设置数字格式，保留一位有效小数
+        df = new DecimalFormat("#0.0");
+        df.setRoundingMode(RoundingMode.HALF_UP);
+        df.setMinimumFractionDigits(1);
+        df.setMaximumFractionDigits(1);
+    }
+
+    public FileServer() throws IOException {
+        super(SERVER_PORT);
+    }
+
+
+    private String getFormatFileSize(long length) {
+        double size = ((double) length) / (1 << 30);
+        if (size >= 1) {
+            return df.format(size) + "GB";
+        }
+        size = ((double) length) / (1 << 20);
+        if (size >= 1) {
+            return df.format(size) + "MB";
+        }
+        size = ((double) length) / (1 << 10);
+        if (size >= 1) {
+            return df.format(size) + "KB";
+        }
+        return length + "B";
+    }
+
+}
